e703da632395e820c69dbd4418da0e11e2cb02aa
Guilherme Macedo <guilherme-macedo@outlook.com>
2024-11-06 10:19:26 -0300

    Update traffic.py

--- program/traffic.py
+++ program/traffic.py
@@ -3,6 +3,14 @@ import sys
 import sklearn
 import tensorflow
 
+
+EPOCHS = 10
+IMAGE_WIDTH = 30
+IMAGE_HEIGHT = 30
+N = 43
+TEST_SIZE = 0.40
+
+
 def load_data(directory):
     '''
         Load image data from directory. Assume 'directory' has one directory named after each category, nubered 0 throught N - 1. Inside each category directory will be some number of image files. Return tuple '(images, labels)'. 'images' should be a list of all the images in the directory, where each image is formatted as numpy ndarray with dimensions IMAGE_WIDTH x IMAGE_HEIGHT x 3. 'labels' shoud be a list of integer labels, represeting the categories for each of the corresponding 'images'.
@@ -27,13 +35,13 @@ if __name__ == '__main__':
     images, labels = load_data(sys.argv[1])
 
     # split data into tranning and testing sets
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(numpy.array(images), numpy.array(labels), test_size = 0.0)
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(numpy.array(images), numpy.array(tensorflow.keras.utils.to_categorical(labels)), test_size = TEST_SIZE)
 
     # get a compiled neural network
     model = get_model()
 
     # fit model on tranning data
-    model.fit(x_train, y_train, epochs = 0)
+    model.fit(x_train, y_train, epochs = EPOCHS)
 
     # evaluate neural network performance
     model.evaluate(x_test, y_test, verbose = 2)
