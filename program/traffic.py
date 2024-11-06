4e6dcfb0092f863df217d4e4966dee214deb5d49
Guilherme Macedo <guilherme-macedo@outlook.com>
2024-11-06 08:36:36 -0300

    Update traffic.py

--- program/traffic.py
+++ program/traffic.py
@@ -1,3 +1,6 @@
+import sys
+import tensorflow
+
 def load_data(directory):
     '''
         Load image data from directory. Assume 'directory' has one directory named after each category, nubered 0 throught N - 1. Inside each category directory will be some number of image files. Return tuple '(images, labels)'. 'images' should be a list of all the images in the directory, where each image is formatted as numpy ndarray with dimensions IMAGE_WIDTH x IMAGE_HEIGHT x 3. 'labels' shoud be a list of integer labels, represeting the categories for each of the corresponding 'images'.
@@ -17,8 +20,10 @@ def main():
     # check command-line arguments
 
     # get image arrays and labels for all image files
+    images, labels = load_data(sys.argv[1])
 
     # split data into tranning and testing sets
+    x_train, x_test, y_train, y_test = None
 
     # get a compiled neural network
 
